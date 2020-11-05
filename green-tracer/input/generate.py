de33a163b60820f69ab556613ef1d8759311ed42
Benjamin Danglot <bdanglot@gmail.com>
2020-11-05 12:19:33 +0100

    input: increase nb generated, increase bound

--- green-tracer/input/generate.py
+++ green-tracer/input/generate.py
@@ -1,5 +1,6 @@
 import random
 
-bound = 1E3
+number = 1E4
+bound = 1E6
 with open('green-tracer/input/input', 'w') as file:
-    file.write('\n'.join([str(random.randint(int(-bound), int(bound))) for x in range(0, int(bound))]))
+    file.write('\n'.join([str(random.randint(int(-bound), int(bound))) for x in range(0, int(number))]))
