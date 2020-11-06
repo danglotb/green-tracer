583a39090c1e1b6a3e92ba61b05a7be70797a1d4
Benjamin Danglot <bdanglot@gmail.com>
2020-11-06 10:26:38 +0100

    fix: run now the given script in the same context

--- green-tracer/green_tracer.py
+++ green-tracer/green_tracer.py
@@ -6,14 +6,18 @@ import os
 class GreenTracer(object):
 
     def traceit(self, frame, event, arg):
-        filename = frame.f_globals["__file__"]
         function_code = frame.f_code
+        filename = function_code.co_filename
         function_name = function_code.co_name
         lineno = frame.f_lineno
-        print('%s %s#%s:%d' % (filename, list(frame.f_globals.keys())[-1], function_name, lineno))
+        if 'python_script_path' in frame.f_globals:
+            filename = frame.f_globals['python_script_path']
+        print('%s %s:%d' % (filename, function_name, lineno))
         return self.traceit
 
 if __name__ == '__main__':
     sys.settrace(GreenTracer().traceit)
-    os.system(' '.join(sys.argv[1:]))
+    python_script_path = sys.argv[1]
+    sys.argv = sys.argv[1:]
+    exec(open(python_script_path).read())
     sys.settrace(None)
