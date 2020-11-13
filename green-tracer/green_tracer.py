9c1c21e26af4b59c72d32b1b7e7b4b8c43d1db39
Benjamin Danglot <bdanglot@gmail.com>
2020-11-13 15:54:47 +0100

    feat: add now the context to the current path, and add some arguments

--- green-tracer/green_tracer.py
+++ green-tracer/green_tracer.py
@@ -2,8 +2,13 @@ import inspect
 import sys
 import linecache
 import os
+import argparse
 
 class GreenTracer(object):
+
+    def __init__(self, output_path):
+        self.output_path = output_path
+        self.output_file = open(self.output_path, 'w')
 
     def traceit(self, frame, event, arg):
         function_code = frame.f_code
@@ -12,12 +17,30 @@ class GreenTracer(object):
         lineno = frame.f_lineno
         if 'python_script_path' in frame.f_globals:
             filename = frame.f_globals['python_script_path']
-        print('%s %s:%d' % (filename, function_name, lineno))
+        self.output_file.write('%s %s:%d\n' % (filename, function_name, lineno))
         return self.traceit
 
+    def close(self):
+        self.output_file.close()
+
 if __name__ == '__main__':
-    sys.settrace(GreenTracer().traceit)
-    python_script_path = sys.argv[1]
-    sys.argv = sys.argv[1:]
-    exec(open(python_script_path).read())
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path-to-script', help='')
+    parser.add_argument('--script', help='')
+    parser.add_argument('--arguments', default='', help='')
+    parser.add_argument('--output-path', help='', default='out.trace')
+    args = parser.parse_args()
+
+    tracer = GreenTracer(args.output_path)
+    script_name = args.script
+    path_to_script = args.path_to_script
+    if not args.arguments == None:
+        arguments = args.arguments.split(',')
+
+    sys.path.append(path_to_script)
+    sys.argv = [path_to_script + script_name] + arguments
+    sys.settrace(tracer.traceit)
+    exec(open(path_to_script + script_name).read())
     sys.settrace(None)
+    tracer.close()
