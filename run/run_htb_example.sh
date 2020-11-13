3f9968fc5cf37908cfa5d9fbfaae0a416ee9d27e
Benjamin Danglot <bdanglot@gmail.com>
2020-11-13 16:14:58 +0100

    refactor: update run_htb_example.sh with new arguments

--- run/run_htb_example.sh
+++ run/run_htb_example.sh
@@ -2,11 +2,10 @@
 
 rm -rf output
 mkdir output
-python3 green-tracer/green_tracer.py example/computation/htbv0.py > output/v0.trace
-python3 example/computation/htbv0_pyjouled.py
-mv result.csv output/result_v0.csv
-python3 green-tracer/green_tracer.py example/computation/htbv1.py > output/v1.trace
-python3 example/computation/htbv1_pyjouled.py
+python3 green-tracer/green_tracer.py --path-to-script example/computation/ --script htbv0.py --output-path output/v0.trace
+#python3 example/computation/htbv0_pyjouled.py
+python3 green-tracer/green_tracer.py --path-to-script example/computation/ --script htbv1.py --output-path output/v1.trace
+#python3 example/computation/htbv1_pyjouled.py
 mv result.csv output/result_v1.csv
 gumtree textdiff example/computation/htbv0.py example/computation/htbv1.py > text.diff
 mv text.diff output/text.diff
