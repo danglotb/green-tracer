d6098fb28814a6c1279b9dced25c6e87dfc0b379
Benjamin Danglot <bdanglot@gmail.com>
2020-11-17 14:59:08 +0100

    feat: do not trace std calls

--- green-tracer/green_tracer.py
+++ green-tracer/green_tracer.py
@@ -3,6 +3,7 @@ import sys
 import linecache
 import os
 import argparse
+import importlib.util
 
 class GreenTracer(object):
 
@@ -10,9 +11,15 @@ class GreenTracer(object):
         self.output_path = output_path
         self.output_file = open(self.output_path, 'w')
 
+    def is_std(self, filename):
+        return filename.startswith('/usr/lib/python3.8') or \
+            filename.startswith('<frozen ')
+
     def traceit(self, frame, event, arg):
         function_code = frame.f_code
         filename = function_code.co_filename
+        if self.is_std(filename):
+            return self.traceit
         function_name = function_code.co_name
         lineno = frame.f_lineno
         if 'python_script_path' in frame.f_globals:
@@ -31,13 +38,11 @@ if __name__ == '__main__':
     parser.add_argument('--arguments', default='', help='')
     parser.add_argument('--output-path', help='', default='out.trace')
     args = parser.parse_args()
-
     tracer = GreenTracer(args.output_path)
     script_name = args.script
     path_to_script = args.path_to_script
     if not args.arguments == None:
         arguments = args.arguments.split(',')
-
     sys.path.append(path_to_script)
     sys.argv = [path_to_script + script_name] + arguments
     sys.settrace(tracer.traceit)
